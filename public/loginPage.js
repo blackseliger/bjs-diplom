2ecbde45ce08af7ed14dd28e684d647fb586108e
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-07-28 17:20:26 +0300

    Перебор с помощью find

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,15 +1,35 @@
 "use strict"
 
 
+
+// loginFormCallback — функция, которая будет выполняться при попытке авторизации
+// registerFormCallback — функция, которая будет выполняться при попытке регистрации
+
 const userForm = new UserForm();
-userForm.loginFormCallback = (data) => {
-    ApiConnector.login(data);
-    if (!data){
-        userForm.setRegisterErrorMessage(`message`)
-    }   else {
-        location.reload();
+userForm.loginFormCallback = function(data) {
+    let authorithAttemp = function(){
+        const correctAccounts = [
+        {login: `oleg@demo.ru`, password: `demo`},
+        {login: `ivan@demo.ru`, password: `demo`},
+        {login: `petr@demo.ru`, password: `demo`},
+        {login: `galina@demo.ru`, password: `demo`},
+        {login: `vladimir@demo.ru`, password: `demo`},
+        ]
+
+        if (correctAccounts.some(element => element.login === data.login && element.password === data.password)){
+            // location.reload();
+            console.log(`все верно`)
+        } else {
+            userForm.setLoginErrorMessage(`Логин или пароль не верны`);
         }
     }
+    
+    ApiConnector.login(data, authorithAttemp);
+    };
+
+
+// userForm.loginFormAction(data);
+
 
 
 userForm.registerFormCallback = (data) => {
@@ -19,6 +39,9 @@ userForm.registerFormCallback = (data) => {
         }   
             location.reload();
 }
+
+
+
 
 // let userForm = new UserForm();
 
