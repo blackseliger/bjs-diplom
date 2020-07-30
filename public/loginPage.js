997ac400036d32c3c5924b9f4380de4223251070
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-07-30 19:09:35 +0300

    Добавил проверку авторизации и регистрации

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,43 +2,26 @@
 
 
 
+
+
+
+
+
 // loginFormCallback — функция, которая будет выполняться при попытке авторизации
 // registerFormCallback — функция, которая будет выполняться при попытке регистрации
 
 const userForm = new UserForm();
 userForm.loginFormCallback = function(data) {
-    let authorithAttemp = function(){
-        data.login = String(data.login);
-        data.password = String(data.password);
-        const correctAccounts = [
-        {login: `oleg@demo.ru`, password: `demo`},
-        {login: `ivan@demo.ru`, password: `demo`},
-        {login: `petr@demo.ru`, password: `demo`},
-        {login: `galina@demo.ru`, password: `demo`},
-        {login: `vladimir@demo.ru`, password: `demo`},
-        ]
-
-        if (correctAccounts.some(element => element.login === data.login && element.password === data.password)){
-            location.reload();
-        } else {
-            userForm.setLoginErrorMessage(`Логин или пароль не верны`);
-        }
-    }
-    
-    ApiConnector.login(data, authorithAttemp);
+    ApiConnector.login(data, response => {
+        return response.success === true ? location.reload() : userForm.setLoginErrorMessage(response.data)
+    });
     };
 
 
-// userForm.loginFormAction(data);
-
-
-
 userForm.registerFormCallback = (data) => {
-   ApiConnector.register(data);
-        if (!data) {
-            userForm.setLoginErrorMessage(`message`)
-        }   
-            location.reload();
+   ApiConnector.register(data, response => {
+       return response.success === true ? location.reload() : userForm.setRegisterErrorMessage(response.userId);
+   });
 }
 
 
