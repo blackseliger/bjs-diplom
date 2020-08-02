1b00e9286ab00751ffe71261c0bce63bbf59356a
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-02 20:30:48 +0300

    Не работает ratesBoard

--- public/homePage.js
+++ public/homePage.js
@@ -2,4 +2,48 @@
 
 
 
+// const { response } = require("express");  // почему когда каждый раз пишу в колбэке слово response, появляется эта строка? что за магия
+
+
+const logoutButton = new LogoutButton();
+
+logoutButton.action = () => {
+    ApiConnector.logout(response => {
+        if (response.success === true){
+            location.reload();
+        };
+    })
+
+    ApiConnector.current( response => {
+        if (response.success === true){
+            ProfileWidget.showProfile(response.data);
+        }
+    })
+}; 
+
+const ratesBoard = new RatesBoard();
+ratesBoard.tableBody = () => {
+    ApiConnector.getStocks(response => {
+        if (response.success === true){
+            ratesBoard.clearTable()
+            ratesBoard.fillTable(response.data);
+        };
+    })
+}
+
+setInterval(ratesBoard.tableBody, 60000);
+
+
+const moneyManager = new MoneyManager();
+moneyManager.addMoneyCallback = (data) =>{
+    ApiConnector.addMoney(data, response => {
+        if (response.success === true ){
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success);
+        } else {
+            moneyManager.setMessage(response.data);
+        }
+    })
+}
+
 
