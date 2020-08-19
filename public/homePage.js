d41b4649935f1502f616433718ec3d6b303549f2
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-08-19 15:52:17 +0300

    Completed all task

--- public/homePage.js
+++ public/homePage.js
@@ -13,35 +13,98 @@ logoutButton.action = () => {
             location.reload();
         };
     })
+};
 
-    ApiConnector.current( response => {
+ApiConnector.current( response => {
         if (response.success === true){
             ProfileWidget.showProfile(response.data);
         }
     })
-}; 
+ 
 
 const ratesBoard = new RatesBoard();
-ratesBoard.tableBody = () => {
+
+let getStocks = () => {
     ApiConnector.getStocks(response => {
         if (response.success === true){
-            ratesBoard.clearTable()
-           return ratesBoard.fillTable(response.data);
-        };
+            ratesBoard.clearTable();
+            return ratesBoard.fillTable(response.data);
+        }
+    })
+}
+getStocks();
+
+setInterval(getStocks, 60000);
+
+let moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, response => {
+        if (response.success === true) {
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, `Пополнение подтверждено`) // вылетает окошко с крысным фоном, так и должно быть?..
+        } else if (response.success === false){
+            moneyManager.setMessage(response.success,response.data)
+        }
+    });
+}
+
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, response => {
+        console.log(response);
+        if (response.success === true){
+            ProfileWidget.showProfile(response.data);
+            moneyManager.setMessage(response.success, `Конвертация подтверждена`) 
+        } else if (response.success === false){
+            moneyManager.setMessage(response.success, response.data);
+        }
     })
 }
 
-setInterval(ratesBoard.tableBody, 1000);
-
-
-const moneyManager = new MoneyManager();
-moneyManager.addMoneyCallback = (data) =>{
-    ApiConnector.addMoney(data, response => {
-        if (response.success === true ){
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney(data, response => {
+        if (response.success === true){
             ProfileWidget.showProfile(response.data);
-            moneyManager.setMessage(response.success);
-        } else {
-            moneyManager.setMessage(response.data);
+            moneyManager.setMessage(response.success, `Перед подтвержден`)
+        } else if (response.success === false){
+            moneyManager.setMessage(response.success, response.data);
+        }
+    })
+}
+
+let favoriteWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(response => {
+    console.log(response)
+    if (response.success === true){
+        favoriteWidget.clearTable();
+        favoriteWidget.fillTable(response.data)
+        moneyManager.updateUsersList(response.data)
+    };
+})
+
+favoriteWidget.addUserCallback  = (data) => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success === true){
+            favoriteWidget.clearTable();
+            favoriteWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoriteWidget.setMessage(response.success, `Пользователь успешно добавлен в список`);
+        } else if (response.success === false){
+            favoriteWidget.setMessage(response.success, response.data);
+        }
+    })
+}
+
+favoriteWidget.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success === true){
+            favoriteWidget.clearTable();
+            favoriteWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data)
+            favoriteWidget.setMessage(response.success, `Пользователь успешно удален из списка`)
+        } else if (response.success === false){
+            favoriteWidget.setMessage(response.success, response.data);
         }
     })
 }
